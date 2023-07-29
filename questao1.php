5ebab2aefbcbb94e2399cc4e7c0362327f0ab15a
Estevao <estevaoribsantos154@gmail.com>
2023-07-29 16:40:04 -0300

    Adicionando parte da programação com todas as condições funcionando

--- questao1.php
+++ questao1.php
@@ -43,5 +43,57 @@
             </div>
         </fieldset>
     </form>
+    <?php
+    $quant = isset($_GET['quant']) ? intval($_GET['quant']) : 0;
+
+    $opselected = isset($_GET['op']) ? $_GET['op'] : null;
+
+    if($quant >= 1 && $quant <= 15){
+        if($opselected != null){
+            if($opselected == "texto"){
+                for($i = 0; $i < $quant; $i++){
+                    echo "<input type=\"text\" name=\"text\$i\" id=\"text\$i\" placeholder=\"Caixa de Texto $i\"><br>";
+                }
+        
+                for($i = 0; $i < $quant; $i++){
+                    echo  "&lt;input type=\"text\" name=\"text$i\" id=\"text$i\" placeholder=\"Caixa de Texto $i\"&gt;<br>";
+                }
+            }
+            else if($opselected == "senha"){
+                for($i = 0; $i < $quant; $i++){
+                    echo "<input type=\"password\" name=\"password\$i\" id=\"password\$i\" placeholder=\"Senha $i\"><br>";
+                }
+        
+                for($i = 0; $i < $quant; $i++){
+                    echo  "&lt;input type=\"password\" name=\"password$i\" id=\"password$i\" placeholder=\"Senha $i\"&gt;<br>";
+                }
+            }
+            else if($opselected == "botao"){
+                for($i = 0; $i < $quant; $i++){
+                    echo "<input type=\"button\" value=\"Botão $i\"><br>";
+                }
+        
+                for($i = 0; $i < $quant; $i++){
+                    echo  "&lt;input type=\"button\" value=\"Botão $i\"&gt;<br>";
+                }
+            }
+            else if($opselected == "radio"){
+                for($i = 0; $i < $quant; $i++){
+                    echo "<input type=\"radio\" name=\"radio\" id=\"\"><br>";
+                }
+        
+                for($i = 0; $i < $quant; $i++){
+                    echo  "&lt;input type=\"button\" value=\"Botão $i\"&gt;<br>";
+                }
+            }
+        }
+        else{
+            echo "Escolha uma opção de entrada!";
+        }
+    }
+    else{
+        echo "A quantidade deve ser maior ou igual a 1 e menor ou igual a 15!";
+    }
+    ?>
 </body>
 </html>
