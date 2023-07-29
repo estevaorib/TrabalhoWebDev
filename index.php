a4984070a63d51e1316c31c1f8336865f64aea7d
Estevao <estevaoribsantos154@gmail.com>
2023-07-29 16:23:11 -0300

    Adicionando estrutura básica da página inicial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <header>
+        <h2>Desenvolvimento Web</h2>
+    </header>
+
+    <main>
+        <h1>Trabalho: Questões</h1>
+
+        <ul>
+            <a href="questao1.php"><li>Questão 1</li></a>
+            <a href="questao2.php"><li>Questão 2</li></a>
+            <a href="questao3.php"><li>Questão 3</li></a>
+            <a href="questao4.php"><li>Questão 4</li></a>
+        </ul>
+    </main>
+
+    <footer>
+        <p>Estevão Ribeiro e Ítalo Augusto - &copy;2023</p>
+    </footer>
+</body>
+</html>
