7614919a66e414bd37202d3fb9ec40cee9a71f62
Estevao <estevaoribsantos154@gmail.com>
2023-08-01 16:07:26 -0300

    Adicionando cores na tabela

--- questao2.php
+++ questao2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Questão 2</title>
+    <link rel="stylesheet" href="../trabalho3trimestre/styles/questao2.css">
 </head>
 <body>
     <header>
@@ -33,28 +34,28 @@
                         for($i = 46; $i <= 120; $i++){
                             $imc =  $i / ($j * $j) * 10000;
                             if($imc <= 16){
-                                echo "<td class=\"muitograve\"";
+                                echo "<td class=\"muitograve\"></td>";
                             }
                             else if($imc > 16 && $imc <= 17){
-                                echo "<td class=\"grave\"";
+                                echo "<td class=\"grave\"></td>";
                             }
                             else if($imc > 17 && $imc <= 18.5){
-                                echo "<td class=\"baixo\"";
+                                echo "<td class=\"baixo\"></td>";
                             }
                             else if($imc > 18.5 && $imc <= 25){
-                                echo "<td class=\"ideal\"";
+                                echo "<td class=\"ideal\"></td>";
                             }
                             else if($imc > 25 && $imc <= 30){
-                                echo "<td class=\"sobrepeso\"";
+                                echo "<td class=\"sobrepeso\"></td>";
                             }
                             else if($imc > 30 && $imc <= 35){
-                                echo "<td class=\"obesidade1\"";
+                                echo "<td class=\"obesidade1\"></td>";
                             }
                             else if($imc > 35 && $imc <= 40){
-                                echo "<td class=\"obesidade2\"";
+                                echo "<td class=\"obesidade2\"></td>";
                             }
                             else if($imc > 40){
-                                echo "<td class=\"obesidade3\"";
+                                echo "<td class=\"obesidade3\"></td>";
                             }
                         }
                         echo "</tr>";
