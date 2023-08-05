50c7b38fab73d3bda277826a65bd930011dc8c96
Ítalo Augusto <107999698+ItaloAugustoFL@users.noreply.github.com>
2023-08-05 15:41:00 +0000

    Criação da questão 3a

---
+++ quest3/quest3a.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+    <link rel="stylesheet" href="../trabalho3trimestre/styles/questao3.css">
+    <link rel="stylesheet" href="../trabalho3trimestre/styles/global.css">
+</head>
+<body>
+    <header>
+        <h2>Desenvolvimento Web</h2>
+    </header>
+
+    <main>
+        <h1>Trabalho: Questão 03</h1>
+    <p><b>Q1. O Ratatoskr, ser/divindade mitológico(a) nórdico(a), vive sua vida onde?</b></p>
+    <ol>
+        <li><a href="quest3b.php?resposta1=A">Em Midgard, junto dos Humanos</a></li>
+        <li><a href="quest3b.php?resposta1=B">Em Asgard, junto dos Deuses merecedores.</a></li>
+        <li><a href="quest3b.php?resposta1=C">Em Álfheim, junto dos Elfos.</a></li>
+        <li><a href="quest3b.php?resposta1=D"class="certa">Na YggDrasil, árvore da vida/do mundo, que interliga os 9 reinos.</a></li>
+        <li><a href="quest3b.php?resposta1=E">Em Jotunheim, o mundo dos gigantes de rocha e de gelo.</a></li>
+        </ol>
+        
+        <p><a href="index.php">Página Inicial</a></p>
+    </main>
+
+    <footer>
+        <p class="copy">Estevão Ribeiro e Ítalo Augusto - &copy;2023</p>
+    </footer>
+</body>
+</html>
